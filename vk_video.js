0c1b2f5aa8242e222e36d61bf6e7ba1567399743
Egor Kulikov <benstiller68@gmail.com>
2019-11-11 00:37:26 +0300

    Add files via upload

---
+++ vk_video.js
@@ -0,0 +1,20 @@
+﻿window.onload = function() {
+	const play = document.getElementsByClassName("videoplayer_media_provider")[0];
+	let timecode_player = localStorage.getItem("timecode_player");
+
+	if (play.baseURI !== ) {
+
+	}
+	const createSaveTimecode = () => {
+
+	}
+	
+	
+	if(play.baseURI === timecode_player.baseURI)
+	if (play) {
+		localStorage.setItem("timecode_player", {
+			time: play.currentTime,
+			uri: play.baseURI
+		});
+	}
+};
