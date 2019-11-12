8f91a42872ab927bb86dcffc3a9c8aec7ebaa322
egor.kulikov <egor.kulikov@yandex.ru>
2019-11-12 13:21:21 +0300

    changes

--- vk_video.js
+++ vk_video.js
@@ -1,20 +1,20 @@
 ﻿window.onload = function() {
+
+	const createSaveTimecode = (play) => {
+		localStorage.setItem("timecode_player", {
+			[play.baseURI]: play.currentTime,
+		});
+	}
+
 	const play = document.getElementsByClassName("videoplayer_media_provider")[0];
 	let timecode_player = localStorage.getItem("timecode_player");
 
-	if (play.baseURI !== ) {
-
+	if (play.baseURI !== Object.values(timecode_player)[0]) {
+		createSaveTimecode(play);
 	}
-	const createSaveTimecode = () => {
 
-	}
 	
 	
-	if(play.baseURI === timecode_player.baseURI)
-	if (play) {
-		localStorage.setItem("timecode_player", {
-			time: play.currentTime,
-			uri: play.baseURI
-		});
-	}
+	if(play.baseURI === timecode_player.uri)
+
 };
